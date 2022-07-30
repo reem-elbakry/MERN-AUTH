057006dc7d2b55083885dd520425691ffdf99eca
reem-elbakry <reemelbakry95@gmail.com>
2022-07-30 05:13:47 +0200

    frontend --forms design

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <div>
+      <Signup />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import background from "./../assets/background.jpg";
+import logo from "./../assets/todo.webp";
+
+const LoginForm = () => {
+  return (
+    <div
+      className="flex items-center justify-center min-h-screen"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "noRepeat",
+      }}
+    >
+      <div class="w-full max-w-xs">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col justify-content items-center">
+          <div className="m-4">
+            <img src={logo} alt="logo" width={50} height={50} />
+          </div>
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              class="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              style={{ backgroundColor: "#60a4b4" }}
+              type="submit"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
